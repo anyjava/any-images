735623fd379354c5a8d5ae539f9088ad144da57e
= <=>
2017-03-28 01:42:02 +0900

    api 에 맞춰 수정

--- src/views/posts/row-items/index.js
+++ src/views/posts/row-items/index.js
@@ -4,7 +4,12 @@ import Template from './index.hbs';
 export default Woowahan.ItemView.create('RowItem', {
   tagName: 'div',
   template: Template,
+
+    viewWillMount(renderData) {
+        renderData.imageSize = renderData.images.length;
+        console.log(renderData);
+    },
   onSelectedRow(event, trigger) {
     trigger({ id: this.getModel('id') });
-  }
+  },
 });
