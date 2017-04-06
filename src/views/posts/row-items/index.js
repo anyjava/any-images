97c03a984093d301e93344bd3ac3a4e55b59af0f
= <=>
2017-04-07 01:22:37 +0900

    여러장의 image 돌려보기 기능 추가

--- src/views/posts/row-items/index.js
+++ src/views/posts/row-items/index.js
@@ -2,14 +2,56 @@ import Woowahan from 'woowahan';
 import Template from './index.hbs';
 
 export default Woowahan.ItemView.create('RowItem', {
-  tagName: 'div',
-  template: Template,
+    tagName: 'div',
+    template: Template,
+
+    currentIndex: 1,
+    totalSize: 0,
+    imageHost: "http://img.anyjava.net/upload/",
+    images: [],
+
+    events: {
+        "click .btn-left": "onLeft",
+        "click .btn-right": "onRight",
+        'click .btn-favorite': 'onFavorite',
+    },
 
     viewWillMount(renderData) {
-        renderData.imageSize = renderData.images.length;
+        this.totalSize = renderData.images.length;
+        renderData.hasAdditionalImage = this.totalSize > 1;
+        renderData.titleImageUrl = this.imageHost + renderData.titleImageUrl;
+
+        this.images = renderData.images;
+
         console.log(renderData);
     },
-  onSelectedRow(event, trigger) {
-    trigger({ id: this.getModel('id') });
-  },
+
+    onSelectedRow(event, trigger) {
+        trigger({ id: this.getModel('id') });
+    },
+
+    onLeft(e) {
+        if (this.currentIndex > 1) {
+            console.log('left');
+            this.currentIndex--;
+            this.replaceImage(this.images[this.currentIndex-1].url);
+        }
+    },
+
+    onRight(e) {
+        if (this.totalSize > this.currentIndex) {
+            console.log('right');
+            this.replaceImage(this.images[this.currentIndex].url);
+            this.currentIndex++;
+        }
+    },
+
+    replaceImage(url) {
+        console.log(url);
+        this.refs.img.src = this.imageHost + url;
+    },
+
+    onFavorite() {
+        alert('comming soon...');
+    },
 });
