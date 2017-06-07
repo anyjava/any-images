f6f00503b50dc76f8ea9ce218199f79cdc4c1651
= <=>
2017-06-08 00:22:00 +0900

    img url 추가

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,12 +1,16 @@
 import Woowahan from 'woowahan';
 import { FETCH_POSTS, ADD_FAVORITE } from '../actions';
 
+// const HOST = 'http://localhost:8080';
+// const HOST = 'http://scrap.api.anyjava.net:8081';
+const HOST = 'http://scrap.api.anyjava.net:8080';
+
 export const fetchPost = Woowahan.Reducer.create(FETCH_POSTS, function(options) {
   this.onSuccess = function(response) {
 		this.finish(response);
   };
 
-  this.getData('http://scrap.api.anyjava.net:8081/articles?size=20&page=' + options.page);
+  this.getData(HOST + '/articles?size=20&page=' + options.page);
 });
 
 export const addFavorite = Woowahan.Reducer.create(ADD_FAVORITE, function (options) {
@@ -14,7 +18,7 @@ export const addFavorite = Woowahan.Reducer.create(ADD_FAVORITE, function (optio
         this.finish(response);
     };
 
-    this.postData('http://scrap.api.anyjava.net:8081/articles/' + options.id + '/favorites', options.data);
+    this.postData(HOST + '/articles/' + options.id + '/favorites', options.data);
 
 });
 
